362fd4e2d4898e2faf1641f563c2197248b4600e
Tobbas <gustobbe@gmail.com>
2021-12-01 16:54:57 +0100

    Update aoc21_1.py

--- aoc21_1.py
+++ aoc21_1.py
@@ -12,11 +12,11 @@ def applic(inpstr):
   if len(dpstore) > 1:
     if int(dpstore[1])-int(dpstore[0]) > 0:
       dpcount += 1
-    else:
-      print('*', end='')
+    #else:
+    #  print('*', end='')
   if len(dpstore) > 2:
     del dpstore[0]
-  print(f'{dpstore} {dpcount}')
+  #print(f'{dpstore} {dpcount}')
 
 def solve():
   with open(inpfile) as inp:
